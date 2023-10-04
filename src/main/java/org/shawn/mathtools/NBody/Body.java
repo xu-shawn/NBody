fc9f4b504b46623316409f8293b821874219161a
Shawn Xu <xu107288696@gmail.com>
2023-10-03 21:27:47 -0700

    Implemented rudimentary velocity support

--- src/main/java/org/shawn/mathtools/NBody/Body.java
+++ src/main/java/org/shawn/mathtools/NBody/Body.java
@@ -6,16 +6,34 @@ public class Body
 	private double x;
 	private double y;
 	private double z;
+
+	private double vx;
+	private double vy;
+	private double vz;
 	
-	public Body(int x, int y, int z)
+	public Body(double x, double y, double z)
+	{
+		this(x, y, z, 0, 0, 0);
+	}
+	
+	public Body(double x, double y, double z, double vx, double vy, double vz)
 	{
 		this.x = x;
 		this.y = y;
 		this.z = z;
+		
+		this.vx = vx;
+		this.vy = vy;
+		this.vz = vz;
 	}
 	
 	public double getDistance(Body other)
 	{
 		return Math.sqrt(x * x + y * y + z * z);
+	}
+	
+	public double getSpeed()
+	{
+		return Math.sqrt(vx * vx + vy * vy + vz * vz);
 	}
 }
