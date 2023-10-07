5e5aec5b6a592eda47bf6893cbd7fb79a04ae8b7
Shawn Xu <xu107288696@gmail.com>
2023-10-06 22:59:24 -0700

    Format demo

--- src/main/java/org/shawn/mathtools/NBody/App.java
+++ src/main/java/org/shawn/mathtools/NBody/App.java
@@ -4,26 +4,27 @@ package org.shawn.mathtools.NBody;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-    	SimpleDisplay mySim = new SimpleDisplay(120, 60);
-    	mySim.getBodyManager().getBodies().add(new Body(5, 0, 0, 0, 100, 0));
-    	mySim.getBodyManager().getBodies().add(new Body(0, 0, 0, 10000));
-    	
-    	while(true)
-    	{
-    		System.out.println("\033[H\033[2J");
-    		System.out.print(mySim);
-    		mySim.update(0.001);
-    		try
-    		{
-    			Thread.sleep(3);
-    		}
-    		catch (InterruptedException e) {
-    			  Thread.currentThread().interrupt();
-    		}
-    	}
-    }
+	public static void main(String[] args)
+	{
+		SimpleDisplay mySim = new SimpleDisplay(120, 60);
+		mySim.getBodyManager().getBodies().add(new Body(5, 0, 0, 0, 100, 0));
+		mySim.getBodyManager().getBodies().add(new Body(0, 0, 0, 10000));
+
+		while (true)
+		{
+			System.out.println("\033[H\033[2J");
+			System.out.print(mySim);
+			mySim.update(0.001);
+			try
+			{
+				Thread.sleep(3);
+			}
+			catch (InterruptedException e)
+			{
+				Thread.currentThread().interrupt();
+			}
+		}
+	}
 }
